0ece5073cbcd29a01b29cf59704c7e5f772ea024
Wellerson Roberto <wellerson.roberto@geohub.com.br>
2019-02-12 18:56:57 -0200

    Revert "Removida mapeamento da interface e polimorfismo no mapeamento"
    This reverts commit 44f049f9645eff55d3e7e6325a63206f2e787232.

--- Models/Map/MonsterMap.cs
+++ Models/Map/MonsterMap.cs
@@ -7,18 +7,13 @@ using System.Threading.Tasks;
 
 namespace Back.Models.Map
 {
-    public class MonsterMap : ClassMap<Monster>
+    public class MonsterMap : SubclassMap<Monster>
     {
         public MonsterMap()
         {
             Schema("yugioh");
             Table("monster");
 
-            Id(x => x.Id).GeneratedBy.HiLo("nhibernate.table_key", "next_value", "1", "type = 'yugioh'.monster");
-            Map(x => x.Name);
-            Map(x => x.Passcode);
-            Map(x => x.Attribute);
-            Map(x => x.Description);
             Map(x => x.ATK);
             Map(x => x.DEF);
             Map(x => x.Level);
