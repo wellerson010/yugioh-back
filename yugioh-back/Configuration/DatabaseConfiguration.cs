09648ffef7dc4c1389ca8706d6b176db053c1606
Wellerson <wellerson.roberto@geopostenergy.com>
2020-05-10 04:08:33 -0300

    Mudando configuração do banco de dados

---
+++ yugioh-back/Configuration/DatabaseConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Model.Services;
+
+namespace Back.Configuration
+{
+    public static class DatabaseConfiguration
+    {
+        public static IConfiguration ConfigureDatabase(this IConfiguration configuration)
+        {
+            string urlDatabase = configuration["Database:Url"];
+            string databaseName = configuration["Database:Name"];
+            string[] urls = { urlDatabase };
+            RavenService.CreateStore(urls, databaseName);
+
+            return configuration;
+        }
+    }
+}
--- yugioh-back/Startup.cs
+++ yugioh-back/Startup.cs
@@ -1,48 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Back.Configuration;
+﻿using Back.Configuration;
 using Back.Middlewares;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Model.Services;
-using Newtonsoft.Json.Serialization;
 
 namespace Back
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
 
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
-
-            ConfigureDatabase();
-        }
-
-        private void ConfigureDatabase()
-        {
-            string urlDatabase = Configuration["Database:Url"];
-            string databaseName = Configuration["Database:Name"];
-            string[] urls = { urlDatabase };
-            RavenService.CreateStore(urls, databaseName);
+            configuration.ConfigureDatabase();
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            //AddScoped - São criados uma vez por solicitação
-            //AddSingleton
-            //AddTransient - são criados qnd solicitados
-
             services.AddMvc().
                 AddJsonOptions(jsonOptions =>
                 {
@@ -72,17 +48,13 @@ namespace Back
             }
 
             app.UseRouting();
-
-         //   app.UseHttpsRedirection();
-
+            app.UseHttpsRedirection();
             app.UseMiddleware<MiddlewareSessionPersistance>();
             app.UseMiddleware<ErrorHandlerMiddleware>();
-
             app.UseCors("CorsPolicy");
             app.UseEndpoints(endPoints=>
             {
                 endPoints.MapControllers();
-                //endPoints.MapControllerRoute(pattern: "default", name: "{controller=Values}/{action=Get}/{id?}");
             });
         }
     }
